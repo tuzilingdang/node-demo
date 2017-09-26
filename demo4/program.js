bc30980dc1889251467eeff8340c17b72c856fbf
April <april@AprildeMacBook-Pro.local>
2017-09-26 23:10:30 +0800

    update demo4

--- demo4/program.js
+++ demo4/program.js
@@ -1,13 +1,14 @@
 
 let readDir = require('./readdir.js');
-
 let folder = process.argv[2];
-let ext = '.' + process.argv[3];
+let ext = process.argv[3];
 
-readDir(folder,  ext, function(err, file) {
+readDir(folder,  ext, function(err, files) {
 	if(err)  return console.log(err);
 
-	console.log(file);
+	files.forEach(function(file){
+		console.log(file);
+	});
 });
 
 
--- demo4/readdir.js
+++ demo4/readdir.js
@@ -2,23 +2,17 @@ let fs = require('fs');
 let path = require('path');
 
 module.exports = function(folder, ext, callback) {
-	// var args = Array.prototype.slice.call(arguments);
-	// let folder = args[0];
-	// let ext = args[1];
-	// let callback = args[2];
-
 	fs.readdir(folder, function(err , files) {
 		if(err) {
 			return callback(err);
 		}
 		debugger;
-		
-		for(let file of files) {
-			debugger;
-			if(path.extname(file) === ext) {
-				callback.call(null, file);
-			}
-		}
+		var fileList = [];
+
+		fileList = files.filter(function(file) {
+			return path.extname(file) === '.' + ext;
+		});
+		callback(null, fileList);
 	})
 
 }
